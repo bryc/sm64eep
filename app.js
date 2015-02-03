29ec56395de4009078b19dd3c76c588f9222fb2c
. <linbrek@gmail.com>
2015-02-03 16:50:29 -0500

    Remove temply, and remove this.controls
    Temply is a templating function, but I won't be needing it

--- app.js
+++ app.js
@@ -113,29 +113,6 @@ window.addEventListener("load", function()
             return (select.length === 1 ? select[0] : select);
         }
         
-        function temply(html, options)
-        {
-            var re = /<%(.+?)%>/g, 
-                reExp = /(^( )?(var|if|for|else|switch|case|break|{|}|;))(.*)?/g, 
-                code = 'with(obj) { var r=[];\n', 
-                cursor = 0, 
-                result;
-            var add = function(line, js) {
-                js? (code += line.match(reExp) ? line + '\n' : 'r.push(' + line + ');\n') :
-                    (code += line != '' ? 'r.push("' + line.replace(/"/g, '\\"') + '");\n' : '');
-                return add;
-            }
-            while(match = re.exec(html)) {
-                add(html.slice(cursor, match.index))(match[1], true);
-                cursor = match.index + match[0].length;
-            }
-            add(html.substr(cursor, html.length - cursor));
-            code = (code + 'return r.join(""); }').replace(/[\r\t\n]/g, '');
-            try { result = new Function('obj', code).apply(options, [options]); }
-            catch(err) { console.error("'" + err.message + "'", " in \n\nCode:\n", code, "\n"); }
-            return result;
-        }
-        
         function initDrag()
         {
             var dragIsFile = false;
@@ -197,7 +174,7 @@ window.addEventListener("load", function()
                 txtBox.oninput  = Eeprom.update;
                 controls.push(txtBox);
                 grab("#out").appendChild(txtBox);
-            } this.controls = controls;
+            }
             updateValues();
         }
 
