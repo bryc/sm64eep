0be4419ea62d502d9377b285cf7199146f9e0f55
. <linbrek@gmail.com>
2015-02-04 18:51:09 -0500

    Tons of code refactoring, some text changes, etc.

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ window.addEventListener("load", function()
     function EepromHandler()
     {
         var _ref = this;
-
+        
         function init()
         {
             for(var i = 0, dat = []; i < 10; i++)
@@ -24,7 +24,7 @@ window.addEventListener("load", function()
             }
             _ref.data = dat;
         }
-
+        
         function updateHex()
         {
             for(var i = 0, t = ""; i < _ref.data.length; i++)
@@ -34,6 +34,32 @@ window.addEventListener("load", function()
                 else { t += digit + " "; }
                 Editor.grab("#out2").innerHTML = t;
             }
+        }
+        
+        function doUpdate()
+        {
+            var md = this.metadata;
+            var size = (md.glob === true) ? 32 : 56;
+            var addr = Editor.addr;
+            
+            if(md.type === "num")
+            {
+                _ref.data[       addr + md.offset] = parseInt(this.value & 0xFF, 10);
+                _ref.data[size + addr + md.offset] = parseInt(this.value & 0xFF, 10);
+            }
+            else if(md.type === "flag")
+            {
+                _ref.data[       addr + md.offset] ^= md.bit;
+                _ref.data[size + addr + md.offset] ^= md.bit;
+            }
+            
+            var sum = sumCheck(addr, size-2, _ref.data);
+            var sum2 = [sum >> 8, sum & 0xFF];
+            
+            _ref.data[addr + size   - 2] = sum2[0];
+            _ref.data[addr + size   - 1] = sum2[1];
+            _ref.data[addr + size*2 - 2] = sum2[0];
+            _ref.data[addr + size*2 - 1] = sum2[1];
         }
         
         function dupeCheck(offset, size, data)
@@ -76,109 +102,37 @@ window.addEventListener("load", function()
             return checkValue;
         }
         
-        function doUpdate()
-        {
-            var md = this.metadata;
-            var size = (md.glob === true) ? 32 : 56;
-            var addr = Editor.addr;
-            if(md.type === "num")
-            {
-                _ref.data[       addr + md.offset] = parseInt(this.value & 0xFF, 10);
-                _ref.data[size + addr + md.offset] = parseInt(this.value & 0xFF, 10);
-            }
-            else if(md.type === "flag")
-            {
-                _ref.data[       addr + md.offset] ^= md.bit;
-                _ref.data[size + addr + md.offset] ^= md.bit;
-            }
-            
-            var sum = sumCheck(addr, size-2, _ref.data);
-            var sum2 = [sum >> 8, sum & 0xFF];
-            _ref.data[addr + size   - 2] = sum2[0];
-            _ref.data[addr + size   - 1] = sum2[1];
-            _ref.data[addr + size*2 - 2] = sum2[0];
-            _ref.data[addr + size*2 - 1] = sum2[1];
-            
-            _ref.updateHex();
-        }
         this.init      = init;
-        this.check     = check;
         this.updateHex = updateHex;
         this.update    = doUpdate;
+        this.check     = check;
     }
     
     function UIHandler()
     {
         var _ref = this, controls = [];
-        this.addr = 0;
         
-        var levelNames = {
-            0x08 : "Castle Interior",
-            0x0C : "Bob-omb Battlefield",
-            0x0D : "Whomp's Fortress",
-            0x0E : "Jolly Roger Bay",
-            0x0F : "Cool, Cool Mountain",
-            0x10 : "Big Boo's Haunt",
-            0x11 : "Hazy Maze Cave",
-            0x12 : "Lethal Lava Land",
-            0x13 : "Shifting Sand Land",
-            0x14 : "Dire, Dire Docks",
-            0x15 : "Snowman's Land",
-            0x16 : "Wet-Dry World",
-            0x17 : "Tall, Tall Mountain",
-            0x18 : "Tiny-Huge Island",
-            0x19 : "Tick Tock Clock",
-            0x1A : "Rainbow Ride",
-            0x1B : "Bowser in the Dark World",
-            0x1C : "Bowser in the Fire Sea",
-            0x1D : "Bowser in the Sky",
-            0x1E : "The Princess's Secret Slide",
-            0x1F : "Cavern of the Metal Cap",
-            0x20 : "Tower of the Wing Cap",
-            0x21 : "Vanish Cap Under the Moat",
-            0x22 : "Wing Mario Over the Rainbow",
-            0x23 : "The Secret Aquarium",
-            0x24 : "unused (0x24)"
-        };
-        
-        var miscFlags = {
-            0x0B : {
-                0 : "Save file occupied",
-                1 : "Wing cap activated",
-                2 : "Metal cap activated",
-                3 : "Vanish cap activated",
-                4 : "Have key (basement)",
-                5 : "Have key (2nd floor)",
-                6 : "Key door unlocked (basement)",
-                7 : "Key door unlocked (2nd floor)"
-            },
-            0x0A : {
-                0 : "Portal moved back (Dire Dire Docks)",
-                1 : "Castle moat drained",
-                2 : "Star door animation seen (Secret Slide)",
-                3 : "Star door animation seen (Whomp)",
-                4 : "Star door animation seen (Cool)",
-                5 : "Star door animation seen (Jolly)",
-                6 : "Star door animation seen (Bowser)",
-                7 : "Star door animation seen (Bowser 2)"
-            },
-            0x09 : {
-                0 : "Lost cap - Level ID (Coordinates)",
-                1 : "Lost cap - Shifting Sand Land",
-                2 : "Lost cap - Tall, Tall Mountain",
-                3 : "Lost cap - Snowman's Land",
-                4 : "Star door animation seen (Third floor/Clock)",
-                5 : "unknown or unused",
-                6 : "unknown or unused",
-                7 : "unknown or unused"
-            }
-        };
-        
-        function grab(a)
+        var grab = function grab(a)
         {
             var select = document.querySelectorAll(a);
             return (select.length === 1 ? select[0] : select);
         }
+        
+        function updateControls()
+        {
+            for(var i = 0; i < controls.length; i++)
+            {
+                if(controls[i].metadata.type === "num")
+                {
+                    controls[i].value = parseInt(Eeprom.data[_ref.addr + controls[i].metadata.offset], 10);
+                }
+                else if(controls[i].metadata.type === "flag")
+                {
+                    controls[i].checked = Eeprom.data[_ref.addr + controls[i].metadata.offset] & controls[i].metadata.bit;
+                }
+            }
+        }
+        
         
         function initDrag()
         {
@@ -194,7 +148,7 @@ window.addEventListener("load", function()
                     isOneItem     = (dT.items.length === 1);
                 dragIsFile = (containsFiles && isOneItem) === true ? true : false;
             });
-    
+            
             window.addEventListener("dragover", function dragover_Check(evt)
             {
                 evt.preventDefault();
@@ -215,44 +169,88 @@ window.addEventListener("load", function()
             });
         }
         
-        function updateValues()
+        function changeSlot()
         {
-            for(var i = 0; i < controls.length; i++)
-            {
-                if(controls[i].metadata.type === "num")
-                {
-                    controls[i].value = parseInt(Eeprom.data[_ref.addr + controls[i].metadata.offset], 10);
-                }
-                else if(controls[i].metadata.type === "flag")
-                {
-                    controls[i].checked = Eeprom.data[_ref.addr + controls[i].metadata.offset] & controls[i].metadata.bit;
-                }
-            }
+            _ref.addr = this.selectedIndex * 112;
+            chrome.storage.local.set({"dataIndex" : _ref.addr});
+            updateControls();
         }
         
         function init()
         {
-            chrome.storage.local.get("dataIndex", function(items)
-            {
-                if(items.dataIndex !== undefined)
-                {
-                    grab("select").selectedIndex = items.dataIndex / 112;
-                    _ref.addr = items.dataIndex;
-                }
-            });
+            var levelNames = {
+                0x08 : "Castle",
+                0x0C : "1 Bob-omb Battlefield",
+                0x0D : "2 Whomp's Fortress",
+                0x0E : "3 Jolly Roger Bay",
+                0x0F : "4 Cool, Cool Mountain",
+                0x10 : "5 Big Boo's Haunt",
+                0x11 : "6 Hazy Maze Cave",
+                0x12 : "7 Lethal Lava Land",
+                0x13 : "8 Shifting Sand Land",
+                0x14 : "9 Dire, Dire Docks",
+                0x15 : "10 Snowman's Land",
+                0x16 : "11 Wet-Dry World",
+                0x17 : "12 Tall, Tall Mountain",
+                0x18 : "13 Tiny-Huge Island",
+                0x19 : "14 Tick Tock Clock",
+                0x1A : "15 Rainbow Ride",
+                0x1B : "Bowser in the Dark World",
+                0x1C : "Bowser in the Fire Sea",
+                0x1D : "Bowser in the Sky",
+                0x1E : "The Princess's Secret Slide",
+                0x1F : "Cavern of the Metal Cap",
+                0x20 : "Tower of the Wing Cap",
+                0x21 : "Vanish Cap Under the Moat",
+                0x22 : "Wing Mario Over the Rainbow",
+                0x23 : "The Secret Aquarium",
+                0x24 : "\"The End\" Screen"
+            };
             
-            var t       = document.createElement("table");
-            t.innerHTML = "<tbody><tr><th>Level</th><th>Level flags</th><th>Coins</th></tr></tbody>";
+            var miscFlags = {
+                0x0B : {
+                    0 : "Save file is occupied on main menu",
+                    1 : "Wing cap activated",
+                    2 : "Metal cap activated",
+                    3 : "Vanish cap activated",
+                    4 : "Have key (basement)",
+                    5 : "Have key (2nd floor)",
+                    6 : "Key door unlocked (basement)",
+                    7 : "Key door unlocked (2nd floor)"
+                },
+                0x0A : {
+                    0 : "Portal moved back (Dire Dire Docks)",
+                    1 : "Castle moat drained",
+                    2 : "Star door animation seen (Secret Slide)",
+                    3 : "Star door animation seen (Whomp)",
+                    4 : "Star door animation seen (Cool)",
+                    5 : "Star door animation seen (Jolly)",
+                    6 : "Star door animation seen (Bowser)",
+                    7 : "Star door animation seen (Bowser 2)"
+                },
+                0x09 : {
+                    0 : "Lost cap - Level ID (Coordinates)",
+                    1 : "Lost cap - Shifting Sand Land",
+                    2 : "Lost cap - Tall, Tall Mountain",
+                    3 : "Lost cap - Snowman's Land",
+                    4 : "Star door animation seen (Third floor/Clock)",
+                    5 : "unknown or unused",
+                    6 : "unknown or unused",
+                    7 : "unknown or unused"
+                }
+            };
+            
+            var levelTable       = document.createElement("table");
+            levelTable.innerHTML = "<tbody><tr><th>Level</th><th>Level flags</th><th>Coins</th></tr></tbody>";
             
             for(var i = 0x08; i <= 0x24; i++)
             {
                 if(i !== 0x09 && i !== 0x0A && i !== 0x0B)
                 {
-                    var fuck = document.createElement('tr');
-                    fuck.innerHTML = "<td>" + levelNames[i] + "</td><td></td><td></td>";
-                    t.childNodes[0].appendChild(fuck); // Append new <tr> to the <tbody>
-                
-                    var y = t.childNodes[0].lastChild; // Get the last added <tr>
+                    var row = document.createElement('tr');
+                    row.innerHTML = "<td>" + levelNames[i] + "</td><td></td><td></td>";
+                    levelTable.childNodes[0].appendChild(row); // Append new <tr> to the <tbody>
+                    var currentRow = levelTable.childNodes[0].lastChild; // Get the last added <tr>
                     
                     for(var j = 0; j < 8; j++)
                     {
@@ -264,7 +262,7 @@ window.addEventListener("load", function()
                             offset : i
                         }
                         o.onchange = Eeprom.update;
-                        y.childNodes[1].appendChild(o);
+                        currentRow.childNodes[1].appendChild(o);
                         controls.push(o);
                     }
                     
@@ -277,13 +275,13 @@ window.addEventListener("load", function()
                             offset : i + 25
                         }
                         o.value = (i+25).toString(16);
-                        y.childNodes[2].appendChild(o);
+                        currentRow.childNodes[2].appendChild(o);
                         controls.push(o);
                     }
                 }
             }
-            grab("#left").appendChild(t);
             
+            var _miscFlags = document.createElement("div");
             for(var i = 0x0B; i >= 0x09; i--)
             {
                 for(var j = 0; j < 8; j++)
@@ -298,30 +296,48 @@ window.addEventListener("load", function()
                         offset : i
                     }
                     o.onchange = Eeprom.update;
-                    p.insertBefore(o,p.firstChild);
-                    grab("#right").appendChild(p);
+                    p.insertBefore(o, p.firstChild);
+                    _miscFlags.appendChild(p);
                     controls.push(o);
                 }
             }
-            updateValues();
+            
+            Eeprom.init();
+            _ref.save.disabled = true;
+            _ref.filename.innerText = "Unsaved file";
+            _ref.initDrag(true); 
+            
+            grab("#lod").onclick    = File.openFile;
+            grab("#sv2").onclick    = File.saveAsFile;
+            grab("#sv1").onclick    = File.saveFile;
+            
+            grab("select").onchange = changeSlot;
+            chrome.storage.local.get("dataIndex", function(items)
+            {
+                if(items.dataIndex !== undefined)
+                {
+                    grab("select").selectedIndex = items.dataIndex / 112;
+                    _ref.addr = items.dataIndex;
+                }
+            });
+            
+            grab("#left").appendChild(levelTable);
+            grab("#right").innerHTML = "<div id=titl>Misc flags</div>";
+            grab("#right").appendChild(_miscFlags);
+            updateControls();
         }
-
-        function select()
-        {
-            _ref.addr = this.selectedIndex * 112;
-            chrome.storage.local.set({"dataIndex" : _ref.addr});
-            updateValues();
-        }
-        this.init = init;
-        this.updateValues = updateValues;
+        
         this.grab = grab;
+        this.addr = 0;
         this.save = grab("#sv1");
-        this.select = select;
         this.filename = grab("#nam");
+        this.updateControls = updateControls;
+        this.changeSlot = changeSlot;
         this.initDrag = initDrag;
+        this.init = init;
     }
     
-    function FileHandler()
+    var FileHandler = function FileHandler()
     {
         var _ref = this;
         
@@ -355,9 +371,7 @@ window.addEventListener("load", function()
                 {
                     _ref.entry = f.entry;
                     Eeprom.data = dat;
-                    Editor.updateValues();
-                    Eeprom.updateHex();
-                    
+                    Editor.updateControls();
                     Editor.save.disabled = false;
                     Editor.filename.innerText = f.file.name;
                     console.log("File loaded: " + f.file.name);
@@ -403,17 +417,5 @@ window.addEventListener("load", function()
     Editor = new UIHandler();
     File   = new FileHandler();
     
-    Eeprom.init();
-    Eeprom.updateHex();
-    Editor.save.disabled = true;
-    Editor.filename.innerText = "Unsaved file";
-    Editor.initDrag(true);
-    
-    Editor.grab("#lod").onclick    = File.openFile;
-    Editor.grab("#sv2").onclick    = File.saveAsFile;
-    Editor.grab("#sv1").onclick    = File.saveFile;
-    Editor.grab("select").onchange = Editor.select;
     Editor.init();
-    
-    //window.ex = function(){ return [Eeprom, Editor, File]; };
 });
