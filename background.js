7a473442f8e507bccfc4db6bed7020ebcafc515e
. <linbrek@gmail.com>
2015-02-03 19:05:39 -0500

    New size, not resizable

--- background.js
+++ background.js
@@ -2,6 +2,7 @@ chrome.app.runtime.onLaunched.addListener(function()
 {
     chrome.app.window.create("index.html",
     {
-        bounds: {width: 600, height: 650}
+        bounds: {width: 712, height: 528},
+        resizable: false
     });
 });
